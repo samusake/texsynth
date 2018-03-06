7dfe7bff5f3790074fa49d1e14b61046b2595889
samusake <samusake@googlemail.com>
2018-03-06 03:07:12 +0100

    modified:   Source/texsynth/FullSynthesis.hx 	modified:   Source/texsynth/PixelData.hx

--- Source/texsynth/FullSynthesis.hx
+++ Source/texsynth/FullSynthesis.hx
@@ -17,53 +17,22 @@ class FullSynthesis{
 		var bestd:Float;
 		var tempd:Float;
 		
-		var startseed:Array<Int> = [];
-		
-		for (j in 0...output.width+2) {
-			startseed.push(cast(Math.random()*0xffffff));
-		}
-		for (j in 0...output.width) {
-			ncolorout = [];
-			ncolorout.push(output.getPixel(j-1,0));
-			ncolorout.push(startseed[j]);
-			ncolorout.push(startseed[j+1]);
-			ncolorout.push(startseed[j+2]);
-			bestd = 195075;
-			for (ki in 0...input.height) {		
-				for (kj in 0...input.width) {	
-					ncolorin = [];
-					ncolorin.push(input.getPixel(kj-1,ki));
-					ncolorin.push(input.getPixel(kj-1,ki-1));
-					ncolorin.push(input.getPixel(kj,ki-1));
-					ncolorin.push(input.getPixel(kj+1,ki-1));
-					tempd = compare(ncolorout, ncolorin);
-					if (tempd < bestd) {
-						bestd = tempd;
-						bestx = kj;
-						besty = ki;	
-					}
-				}
-			}
-			output.setPixel(j, 0, input.getPixel(bestx, besty));
-		}
-
-
-		for (i in 1...output.height) {
+		for (i in 0...output.height) {
 			trace("y= " +i);
 			for (j in 0...output.width) {
 				ncolorout = [];
-				ncolorout.push(output.getPixel(j-1,i));
-				ncolorout.push(output.getPixel(j-1,i-1));
-				ncolorout.push(output.getPixel(j,i-1));
-				ncolorout.push(output.getPixel(j+1,i-1));
+				ncolorout.push(output.getPixelSeamless(j-1,i));
+				ncolorout.push(output.getPixelSeamless(j-1,i-1));
+				ncolorout.push(output.getPixelSeamless(j,i-1));
+				ncolorout.push(output.getPixelSeamless(j+1,i-1));
 				bestd = 195075;
 				for (ki in 0...input.height) {		
 					for (kj in 0...input.width) {	
 						ncolorin = [];
-						ncolorin.push(input.getPixel(kj-1,ki));
-						ncolorin.push(input.getPixel(kj-1,ki-1));
-						ncolorin.push(input.getPixel(kj,ki-1));
-						ncolorin.push(input.getPixel(kj+1,ki-1));
+						ncolorin.push(input.getPixelSeamless(kj-1,ki));
+						ncolorin.push(input.getPixelSeamless(kj-1,ki-1));
+						ncolorin.push(input.getPixelSeamless(kj,ki-1));
+						ncolorin.push(input.getPixelSeamless(kj+1,ki-1));
 						tempd = compare(ncolorout, ncolorin);
 						if (tempd < bestd) {
 							bestd = tempd;
