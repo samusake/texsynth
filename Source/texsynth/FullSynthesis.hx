a20dcef576fe879c712c9f3df35a752736af6185
samusake <samusake@googlemail.com>
2018-03-06 03:54:28 +0100

    modified:   Source/MainOpenfl.hx 	modified:   Source/texsynth/FullSynthesis.hx

--- Source/texsynth/FullSynthesis.hx
+++ Source/texsynth/FullSynthesis.hx
@@ -2,14 +2,11 @@ package texsynth;
 
 class FullSynthesis{
 	
-	public static function render(input:PixelData<ARGB>, width:Int, height:Int):PixelData<ARGB> {
+	public static function render(input:PixelData<ARGB>, width:Int, height:Int, neighborhood_x:Int=2, neighborhood_y:Int=2, passes:Int=1):PixelData<ARGB> {
 		
 		var output = new PixelData<ARGB>(width, height);
 		
 		output.randomize();
-		
-		var ncolorout:Array<ARGB>;
-		var ncolorin:Array<ARGB>;
 		
 		var bestx:Int = 0;
 		var besty:Int = 0;
@@ -17,45 +14,35 @@ class FullSynthesis{
 		var bestd:Float;
 		var tempd:Float;
 		
-		for (i in 0...output.height) {
-			trace("y= " +i);
-			for (j in 0...output.width) {
-				ncolorout = [];
-				ncolorout.push(output.getPixelSeamless(j-1,i));
-				ncolorout.push(output.getPixelSeamless(j-1,i-1));
-				ncolorout.push(output.getPixelSeamless(j,i-1));
-				ncolorout.push(output.getPixelSeamless(j+1,i-1));
-				bestd = 195075;
-				for (ki in 0...input.height) {		
-					for (kj in 0...input.width) {	
-						ncolorin = [];
-						ncolorin.push(input.getPixelSeamless(kj-1,ki));
-						ncolorin.push(input.getPixelSeamless(kj-1,ki-1));
-						ncolorin.push(input.getPixelSeamless(kj,ki-1));
-						ncolorin.push(input.getPixelSeamless(kj+1,ki-1));
-						tempd = compare(ncolorout, ncolorin);
-						if (tempd < bestd) {
-							bestd = tempd;
-							bestx = kj;
-							besty = ki;	
+		for(p in 0...passes){
+			for (i in 0...output.height) {
+				trace("y= " +i);
+				for (j in 0...output.width) {
+					bestd = 195075;
+					for (ki in 0...input.height) {		
+						for (kj in 0...input.width) {	
+							tempd=0;
+							for(nx in 1...neighborhood_x+1){
+								tempd += input.getPixelSeamless(kj-nx, ki).absErrorNorm2(output.getPixelSeamless(j-nx, i));
+							}
+							for(nx in (0-neighborhood_x)...neighborhood_x+1){
+								for(ny in 1...neighborhood_y+1){
+									tempd += input.getPixelSeamless(kj-nx, ki-ny).absErrorNorm2(output.getPixelSeamless(j-nx, i-ny));
+								}
+							}
+							if (tempd < bestd) {
+								bestd = tempd;
+								bestx = kj;
+								besty = ki;	
+							}
 						}
 					}
+					output.setPixel(j, i, input.getPixel(bestx, besty));
 				}
-				output.setPixel(j, i, input.getPixel(bestx, besty));
 			}
 		}
 		
 		return output;
 	}
-	
-	static inline function compare(ncout:Array<ARGB>, ncin:Array<ARGB>):Float {
-		var d:Float=0;
-		for (i in 0...ncout.length)
-			d += ncin[i].absErrorNorm2( ncout[i] );
-			
-		return(d);
-	}
-	
-	
 	
 }
