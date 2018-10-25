e8a83a0badddb392fb8189efe501562827649f44
Sylvio Sell <info@maitag.de>
2018-10-25 07:47:17 +0200

    fix benchmark.sh for older shell

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo
 
-haxe benchmarks.hxml &>/dev/null
+haxe benchmarks.hxml >/dev/null
 echo testing with Vector:
 echo 
 echo ------------------- Node -------------------------------------
@@ -16,7 +16,7 @@ echo
 echo
 echo
 
-haxe -D PixelMatrixUseArray benchmarks.hxml &>/dev/null
+haxe -D PixelMatrixUseArray benchmarks.hxml >/dev/null
 echo testing with Array:
 echo 
 echo ------------------- Node -------------------------------------
@@ -31,7 +31,7 @@ echo
 echo
 echo
 
-haxe -D PixelMatrixUseUInt32Array benchmarks.hxml &>/dev/null
+haxe -D PixelMatrixUseUInt32Array benchmarks.hxml >/dev/null
 echo testing with UInt32Array:
 echo 
 echo ------------------- Node -------------------------------------
@@ -46,7 +46,7 @@ echo
 echo
 echo
 
-haxe -D PixelMatrixUseBytes benchmarks.hxml &>/dev/null
+haxe -D PixelMatrixUseBytes benchmarks.hxml >/dev/null
 echo testing with Bytes:
 echo 
 echo ------------------- Node -------------------------------------
